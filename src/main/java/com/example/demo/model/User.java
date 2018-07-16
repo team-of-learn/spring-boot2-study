d3237d4dd032e2a591f0061f04acab8a8d89f92b
Lixin <1109922834@qq.com>
2018-07-16 16:32:32 +0800

    start

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@RequiredArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    private  Long id; //ID
+    @NonNull private  String name; //姓名
+    @NonNull private  String jobNumber; //工号
+    @NonNull
+    private Date createTime; //创建时间
+}
