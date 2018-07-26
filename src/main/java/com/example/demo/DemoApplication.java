3bef5af6d6b835d788b25db4b55abb8c6f453fd7
Lixin <1109922834@qq.com>
2018-07-26 14:37:02 +0800

    boot-study-update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,17 +5,25 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DemoApplication.class);
+    }
+	
 	@Bean
 	InitializingBean initData(UserRepository repo){
 		return ()->{
--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -8,28 +8,65 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
-@RequestMapping("/user")
+//@RequestMapping(value="/user")
 @Slf4j
 public class UserController {
 
       @ModelAttribute
       public void populateModel(Model model) {
-          model.addAttribute("title", "用户列表");
+          model.addAttribute("title1", "用户列表");
+          model.addAttribute("title2", "修改用户");
       }
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping()
+    @GetMapping(value="/list")
     public String list(Model model) throws Exception {
         List<User> userList = userRepository.findAll();
         model.addAttribute("userList", userList);
         return "/user/list";
     }
+    
+//  public List<User> userlist(){
+//	return userRepository.findAll();
+//}
+    
+    @GetMapping(value = "/update/{id}")
+    public String selectone(@PathVariable("id") Long id,Model model) {
+    	User user = userRepository.getOne(id);
+    	model.addAttribute("user", user);
+    	return "/user/update";
+    }
+    
+    
+    
+    @GetMapping(value = "/save")
+	public String update(@RequestParam("id") Long id, 
+					   @RequestParam("name") String name,
+					   @RequestParam("jobNumber") String jobNumber,Model model) {
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setJobNumber(jobNumber);
+		user.setCreateTime(new Date());
+		userRepository.save(user);
+		List<User> userList = userRepository.findAll();
+        model.addAttribute("userList", userList);
+        return "/user/list";
+	}
+    
 }
